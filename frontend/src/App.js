c3fb745e2871c2b045966182faeffebedf1d8a11
Nicol <nvojacek@gmail.com>
2018-11-19 11:56:15 +0200

    Rename frontend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p> Heelo</p>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
