fba4fb0770ca41ad13173f5faebe7d881285bb61
Nicol <nvojacek@gmail.com>
2018-11-21 12:27:15 +0200

    remove example

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,24 +3,6 @@ from recipe_book import RecipeBook
 from recipe import Recipe
 
 
-# @click.command()
-# @click.option('--count', default=1, help='Number of greetings.')
-# @click.option('--name', prompt='Your name',
-#               help='The person to greet.')
-# def hello(count, name):
-#     """Simple program that greets NAME for a total of COUNT times."""
-#     for x in range(count):
-#         click.echo('Hello %s!' % name)
-
-# if __name__ == '__main__':
-#     hello()
-
-# $ python hello.py --count=3
-# Your name: John
-# Hello John!
-# Hello John!
-# Hello John!
-
 @click.command()
 @click.option('--add_recipe', prompt='Name', help='Add new recipe with ingredients and instructions.')
 def main(add_recipe):
